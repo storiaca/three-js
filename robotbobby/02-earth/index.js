e713eea46aa3ed0239e3bf9525f3f52057d44ca9
Aleksandar Ristic <storiaca@gmail.com>
2024-06-16 10:59:25 +0200

    Earth finished

--- robotbobby/02-earth/index.js
+++ robotbobby/02-earth/index.js
@@ -13,24 +13,74 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+scene.add(earthGroup);
+
+new OrbitControls(camera, renderer.domElement);
+
+const detail = 12;
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xffff00,
+  map: loader.load("./textures/00_earthmap1k.jpg"),
+  //flatShading: true,
 });
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
 
-const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
-scene.add(hemiLight);
+const lightsMat = new THREE.MeshBasicMaterial({
+  // color: 0x00ff00,
+  // transparent: true,
+  // opacity: 0.6,
+  map: loader.load("./textures/03_earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightMesh);
+
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/04_earthcloudmap.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.003);
+earthGroup.add(cloudsMesh);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
+
+const stars = getStarfield({ numStars: 2000 });
+scene.add(stars);
+
+// const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+// scene.add(hemiLight);
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2, 0.5, 1);
+scene.add(sunLight);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.02;
+  earthMesh.rotation.y += 0.002;
+  lightMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.0023;
+  glowMesh.rotation.y += 0.002;
 
   renderer.render(scene, camera);
 }
 
 animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleWindowResize, false);
--- robotbobby/README.md
+++ robotbobby/README.md
@@ -23,3 +23,11 @@ https://github.com/bobbyroe/transition-effect
 ## Add Physics to Three.js with Rapier
 
 https://github.com/bobbyroe/physics-with-rapier-and-three
+
+## IcosahedronGeometry
+
+https://threejs.org/docs/index.html?q=Icosahedron#api/en/geometries/IcosahedronGeometry
+
+## Planetary Pixel Emporium
+
+https://planetpixelemporium.com/
