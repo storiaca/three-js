b9d92f19b56b66a1cc4535fd758303b7a5edd77d
Aleksandar Ristic <storiaca@gmail.com>
2024-06-27 12:43:44 +0200

    Shaders start

--- practice/05-extra/46-performance/src/script.js
+++ practice/05-extra/46-performance/src/script.js
@@ -87,7 +87,7 @@ const cube = new THREE.Mesh(
 cube.castShadow = true;
 cube.receiveShadow = true;
 cube.position.set(-5, 0, 0);
-scene.add(cube);
+//scene.add(cube);
 
 const torusKnot = new THREE.Mesh(
   new THREE.TorusKnotGeometry(1, 0.4, 128, 32),
@@ -95,7 +95,7 @@ const torusKnot = new THREE.Mesh(
 );
 torusKnot.castShadow = true;
 torusKnot.receiveShadow = true;
-scene.add(torusKnot);
+//scene.add(torusKnot);
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(1, 32, 32),
@@ -104,7 +104,7 @@ const sphere = new THREE.Mesh(
 sphere.position.set(5, 0, 0);
 sphere.castShadow = true;
 sphere.receiveShadow = true;
-scene.add(sphere);
+//scene.add(sphere);
 
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
@@ -114,18 +114,18 @@ floor.position.set(0, -2, 0);
 floor.rotation.x = -Math.PI * 0.5;
 floor.castShadow = true;
 floor.receiveShadow = true;
-scene.add(floor);
+//scene.add(floor);
 
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 3, 2.25);
-scene.add(directionalLight);
+// const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.mapSize.set(1024, 1024);
+// directionalLight.shadow.camera.far = 15;
+// directionalLight.shadow.normalBias = 0.05;
+// directionalLight.position.set(0.25, 3, 2.25);
+// scene.add(directionalLight);
 
 /**
  * Animate
@@ -166,32 +166,32 @@ console.log(renderer.info);
 // cube.material.dispose()
 
 // // Tip 10
-directionalLight.shadow.camera.top = 3;
-directionalLight.shadow.camera.right = 6;
-directionalLight.shadow.camera.left = -6;
-directionalLight.shadow.camera.bottom = -3;
-directionalLight.shadow.camera.far = 10;
-directionalLight.shadow.mapSize.set(1024, 1024);
+// directionalLight.shadow.camera.top = 3;
+// directionalLight.shadow.camera.right = 6;
+// directionalLight.shadow.camera.left = -6;
+// directionalLight.shadow.camera.bottom = -3;
+// directionalLight.shadow.camera.far = 10;
+// directionalLight.shadow.mapSize.set(1024, 1024);
 
-const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-scene.add(cameraHelper);
+// const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(cameraHelper);
 
-// // Tip 11
-cube.castShadow = true;
-cube.receiveShadow = false;
+// // // Tip 11
+// cube.castShadow = true;
+// cube.receiveShadow = false;
 
-torusKnot.castShadow = true;
-torusKnot.receiveShadow = false;
+// torusKnot.castShadow = true;
+// torusKnot.receiveShadow = false;
 
-sphere.castShadow = true;
-sphere.receiveShadow = false;
+// sphere.castShadow = true;
+// sphere.receiveShadow = false;
 
-floor.castShadow = false;
-floor.receiveShadow = true;
+// floor.castShadow = false;
+// floor.receiveShadow = true;
 
-// // Tip 12
-renderer.shadowMap.autoUpdate = false;
-renderer.shadowMap.needsUpdate = true;
+// // // Tip 12
+// renderer.shadowMap.autoUpdate = false;
+// renderer.shadowMap.needsUpdate = true;
 
 // // Tip 18
 // const geometries = [];
@@ -250,103 +250,102 @@ renderer.shadowMap.needsUpdate = true;
 // }
 
 // // Tip 22
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
-const material = new THREE.MeshNormalMaterial();
+// const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.InstancedMesh(geometry, material, 50);
-mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
-scene.add(mesh);
+// const mesh = new THREE.InstancedMesh(geometry, material, 50);
+// mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+// scene.add(mesh);
 
-for (let i = 0; i < 50; i++) {
-  const position = new THREE.Vector3(
-    (Math.random() - 0.5) * 10,
-    (Math.random() - 0.5) * 10,
-    (Math.random() - 0.5) * 10
-  );
+// for (let i = 0; i < 50; i++) {
+//   const position = new THREE.Vector3(
+//     (Math.random() - 0.5) * 10,
+//     (Math.random() - 0.5) * 10,
+//     (Math.random() - 0.5) * 10
+//   );
 
-  const quaternion = new THREE.Quaternion();
-  quaternion.setFromEuler(
-    new THREE.Euler(
-      (Math.random() - 0.5) * Math.PI * 2,
-      (Math.random() - 0.5) * Math.PI * 2,
-      0
-    )
-  );
+//   const quaternion = new THREE.Quaternion();
+//   quaternion.setFromEuler(
+//     new THREE.Euler(
+//       (Math.random() - 0.5) * Math.PI * 2,
+//       (Math.random() - 0.5) * Math.PI * 2,
+//       0
+//     )
+//   );
 
-  const matrix = new THREE.Matrix4();
-  matrix.makeRotationFromQuaternion(quaternion);
-  matrix.setPosition(position);
+//   const matrix = new THREE.Matrix4();
+//   matrix.makeRotationFromQuaternion(quaternion);
+//   matrix.setPosition(position);
 
-  mesh.setMatrixAt(i, matrix);
-  //   const mesh = new THREE.Mesh(geometry, material);
-  //   mesh.position.x = (Math.random() - 0.5) * 10;
-  //   mesh.position.y = (Math.random() - 0.5) * 10;
-  //   mesh.position.z = (Math.random() - 0.5) * 10;
-  //   mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2;
-  //   mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2;
-}
+//   mesh.setMatrixAt(i, matrix);
+//   //   const mesh = new THREE.Mesh(geometry, material);
+//   //   mesh.position.x = (Math.random() - 0.5) * 10;
+//   //   mesh.position.y = (Math.random() - 0.5) * 10;
+//   //   mesh.position.z = (Math.random() - 0.5) * 10;
+//   //   mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2;
+//   //   mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2;
+// }
 
 // // Tip 29
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // // Tip 31, 32, 34 and 35
-// const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256)
+const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256);
 
-// const shaderMaterial = new THREE.ShaderMaterial({
-//     uniforms:
-//     {
-//         uDisplacementTexture: { value: displacementTexture },
-//         uDisplacementStrength: { value: 1.5 }
-//     },
-//     vertexShader: `
-//         uniform sampler2D uDisplacementTexture;
-//         uniform float uDisplacementStrength;
+const shaderMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    uDisplacementTexture: { value: displacementTexture },
+    uDisplacementStrength: { value: 1.5 },
+  },
+  vertexShader: `
+        uniform sampler2D uDisplacementTexture;
+        uniform float uDisplacementStrength;
 
-//         varying vec2 vUv;
+        varying vec2 vUv;
 
-//         void main()
-//         {
-//             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+        void main()
+        {
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-//             float elevation = texture2D(uDisplacementTexture, uv).r;
-//             if(elevation < 0.5)
-//             {
-//                 elevation = 0.5;
-//             }
+            float elevation = texture2D(uDisplacementTexture, uv).r;
+            if(elevation < 0.5)
+            {
+                elevation = 0.5;
+            }
 
-//             modelPosition.y += elevation * uDisplacementStrength;
+            modelPosition.y += elevation * uDisplacementStrength;
 
-//             gl_Position = projectionMatrix * viewMatrix * modelPosition;
+            gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
-//             vUv = uv;
-//         }
-//     `,
-//     fragmentShader: `
-//         uniform sampler2D uDisplacementTexture;
+            vUv = uv;
+        }
+    `,
+  fragmentShader: `
+        uniform sampler2D uDisplacementTexture;
 
-//         varying vec2 vUv;
+        varying vec2 vUv;
 
-//         void main()
-//         {
-//             float elevation = texture2D(uDisplacementTexture, vUv).r;
-//             if(elevation < 0.25)
-//             {
-//                 elevation = 0.25;
-//             }
+        void main()
+        {
+            float elevation = texture2D(uDisplacementTexture, vUv).r;
+            if(elevation < 0.25)
+            {
+                elevation = 0.25;
+            }
 
-//             vec3 depthColor = vec3(1.0, 0.1, 0.1);
-//             vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
-//             vec3 finalColor = vec3(0.0);
-//             finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
-//             finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
-//             finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
+            vec3 depthColor = vec3(1.0, 0.1, 0.1);
+            vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
+            vec3 finalColor = vec3(0.0);
+            finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
+            finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
+            finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
 
-//             gl_FragColor = vec4(finalColor, 1.0);
-//         }
-//     `
-// })
+            gl_FragColor = vec4(finalColor, 1.0);
+        }
+    `,
+});
 
-// const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial)
-// shaderMesh.rotation.x = - Math.PI * 0.5
-// scene.add(shaderMesh)
+const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial);
+shaderMesh.rotation.x = -Math.PI * 0.5;
+scene.add(shaderMesh);
